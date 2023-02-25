b0acedb6d5fe49fa80c5c15638852b803c76898a
JacobMaurice-jm <109557146+JacobMaurice-jm@users.noreply.github.com>
2023-02-25 16:26:06 -0500

    JQuery complete setup

---
+++ Lab-5/scripts/form.js
@@ -0,0 +1,12 @@
+$('#myModal').modalSteps();
+
+$('#myModal').modalSteps({
+    btnCancelHtml: "Cancel",
+    btnPreviousHtml: "Previous",
+    btnNextHtml: "Next",
+    btnLastStepHtml: "Complete",
+    disableNextButton: false,
+    completeCallback: function(){},
+    callbacks: {},
+    getTitleAndStep: function() {}
+    });
